d2ebf4c6a1fe1eb5cecc689134c10009313222a4
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2021-06-18 18:36:36 -0400

    Final Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'e-commerce';
+  title = 'Student Details';
+  // url of students data
+  stu_data_api='https://api.hatchways.io/assessment/students'
+   students;
+   Name;
+   // http service for api call or we can create a service and call the function here also
+    constructor(private http:HttpClient){
+    
+    }
+    ngOnInit() {
+      // api call to get the json data
+      this.http.get(this.stu_data_api).subscribe(
+        val => this.students = val['students']
+      ) 
+    }
+
+    search(){
+      if(this.Name==""){
+        this.ngOnInit();
+      }
+      else{
+        console.log(this.Name)
+      this.students = this.students.filter(res=>{
+        // this returns the student that match with the name entered in search box
+        return res.firstName.toLowerCase().match(this.Name.toLowerCase()) || res.lastName.toLowerCase().match(this.Name.toLowerCase()) ;
+      });
+      }
+      }
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { StudentCardComponent } from './student-card/student-card.component';
+import { AveragePipe } from './pipes/average.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentCardComponent,
+    AveragePipe,
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
